26598cd4ca2d0ae139e703fa3338c5e38b7cb54b
Omar Hosny <omar2108721@miuegypt.edu.eg>
2023-05-12 17:52:32 +0200

    slideshow & offers dynamic data

--- app.js
+++ app.js
@@ -11,9 +11,57 @@ const cat = [
   { name: "Kitchen & appliances", imageUrl: "/images/kitchen-appliances.jpg" },
   { name: "Bathroom products", imageUrl: "/images/bathroom-products.jpg" },
 ];
+const offers = [
+  {
+    name: "Product 1",
+    image: "/images/chair2.jpg",
+    description: "Product 1 Description",
+    oldPrice: "$99.99",
+    newPrice: "$79.99",
+  },
+  {
+    name: "Product 2",
+    image: "/images/chair2.jpg",
+    description: "Product 2 Description",
+    oldPrice: "$149.99",
+    newPrice: "$119.99",
+  },
+  {
+    name: "Product 3",
+    image: "/images/chair2.jpg",
+    description: "Product 3 Description",
+    oldPrice: "$199.99",
+    newPrice: "$120",
+  },
+  {
+    name: "Product 4",
+    image: "/images/chair2.jpg",
+    description: "Product 4 Description",
+    oldPrice: "$79.99",
+    newPrice: "$59.99",
+  },
+];
+
+const slides = [
+  {
+    imageUrl: "/images/GM-BOXX-04-3-large (1).jpg",
+    title: "Furniture",
+    subtitle: "Create Your Home",
+  },
+  {
+    imageUrl: "/images/61tV07vXkIL._SL1173_.jpg",
+    title: "Furniture",
+    subtitle: "Create Your Home",
+  },
+  {
+    imageUrl: "/images/living-wall306.jpeg",
+    title: "Furniture",
+    subtitle: "Create Your Home",
+  },
+];
 
 app.get("/", function (req, res) {
-  res.render("index", { cat });
+  res.render("index", { cat: cat, offers: offers, slides: slides });
 });
 app.get("/form.ejs", function (req, res) {
   res.render("form");
@@ -31,9 +79,3 @@ app.get("/cartpage.ejs", function (req, res) {
 app.get("/category.ejs", function (req, res) {
   res.render("category");
 });
-
-const employees = [
-  { id: "1", name: "Essam Eliwa", position: "Doctor" },
-  { id: "2", name: "Nada Ayman", position: "Assistant" },
-  { id: "3", name: "XYZ", position: "ABC" },
-];
