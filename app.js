0fe036f28f153eacbcee758a3cc69e6def40526e
Omar Hosny <omar2108721@miuegypt.edu.eg>
2023-05-12 16:12:52 +0200

    homepage paths

--- app.js
+++ app.js
@@ -8,3 +8,19 @@ app.use(express.static("public"));
 app.get("/", function (req, res) {
   res.render("index");
 });
+app.get("/form.ejs", function (req, res) {
+  res.render("form");
+});
+
+app.get("/category.ejs", function (req, res) {
+  res.render("category");
+});
+app.get("/Product-page.ejs", function (req, res) {
+  res.render("Product-page");
+});
+app.get("/cartpage.ejs", function (req, res) {
+  res.render("cart-page");
+});
+app.get("/category.ejs", function (req, res) {
+  res.render("category");
+});
