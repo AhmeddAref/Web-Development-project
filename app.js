9a86155eed726df177a8dfbc9a62920428436210
Omar Hosny <omar2108721@miuegypt.edu.eg>
2023-05-12 17:32:41 +0200

    category dynamic data

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ const app = express();
 app.listen(9999);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+const cat = [
+  { name: "Beds & mattresses", imageUrl: "/images/beds-mattresses.jpg" },
+  { name: "Furniture", imageUrl: "/images/furniture.jpg" },
+  { name: "Decoration", imageUrl: "/images/decoration.jpg" },
+  { name: "Kitchen & appliances", imageUrl: "/images/kitchen-appliances.jpg" },
+  { name: "Bathroom products", imageUrl: "/images/bathroom-products.jpg" },
+];
 
 app.get("/", function (req, res) {
-  res.render("index");
+  res.render("index", { cat });
 });
 app.get("/form.ejs", function (req, res) {
   res.render("form");
@@ -24,3 +31,9 @@ app.get("/cartpage.ejs", function (req, res) {
 app.get("/category.ejs", function (req, res) {
   res.render("category");
 });
+
+const employees = [
+  { id: "1", name: "Essam Eliwa", position: "Doctor" },
+  { id: "2", name: "Nada Ayman", position: "Assistant" },
+  { id: "3", name: "XYZ", position: "ABC" },
+];
