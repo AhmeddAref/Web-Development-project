b2572544155cb52e9e996a635b4e736af36bae5d
Mohammad Hisham <mohammad2109652@miuegypt.edu.eg>
2023-05-13 15:03:59 +0300

    product page dynamic

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 import express from "express";
-import session from "express-session";
+
 const app = express();
 
 app.listen(9999);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(
-  session({
-    secret: "your_secret_key",
-    resave: false,
-    saveUninitialized: false,
-  })
-);
+
 //-------------------------------------------//
 const cat = [
   { name: "Beds & mattresses", imageUrl: "/images/beds-mattresses.jpg" },
@@ -105,6 +99,26 @@ cart.tax = cart.subtotal * 0.05;
 cart.total = cart.subtotal + cart.tax + cart.shipping;
 //-------------------------------------------//
 
+const prod = {
+  name: "Modern chair",
+  rate: "4.2(21)",
+  image1: "/images/chair1.jpg",
+  image2: "/images/chair2.jpg",
+  image3: "/images/chair3.jpg",
+  image4: "/images/chair4.jpg",
+  description:
+    "If you like the stylish airy look, you have to try the deep generous seats. Create your own personal combination of SÖDERHAMN sofa, then sit down and relax – by yourself or together with the whole family.",
+  color: "Black",
+  Available: "in stock",
+  Category: "Modern chair",
+  Shipping_Area: "All over the world",
+  Shipping_Fee: "Free",
+  oldPrice: "$2570.00",
+  newPrice: "$2490.00 (5%)",
+};
+
+//-----------------------------------//
+
 app.get("/", function (req, res) {
   res.render("index", { cat: cat, offers: offers, slides: slides });
 });
@@ -116,7 +130,7 @@ app.get("/category.ejs", function (req, res) {
   res.render("category");
 });
 app.get("/Product-page.ejs", function (req, res) {
-  res.render("Product-page");
+  res.render("Product-page", { prod: prod });
 });
 app.get("/cart-page.ejs", function (req, res) {
   res.render("cart-page", { cart: cart });
