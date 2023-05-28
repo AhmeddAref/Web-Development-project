8a278e0b7fd194f09f63b3faef925bceafe65aab
Mohammad Hisham <mohammad2109652@miuegypt.edu.eg>
2023-05-28 15:19:24 +0300

    admin privacy

--- routes/Admin-page.js
+++ routes/Admin-page.js
@@ -1,10 +1,23 @@
 import { Router } from "express";
 var router = Router();
 
+// check if admin
+router.use((req, res, next) => {
+  if (req.session.email !== undefined && req.session.email.Type === "admin") {
+    next();
+  } else {
+    res.render("./", {
+      err: "You are not an Admin",
+      user: req.session.email === undefined ? "" : req.session.email,
+    });
+  }
+});
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("Admin-page", {
-    Email: req.session && req.session.Email !== undefined ? req.session.Email : "",
+    Email:
+      req.session && req.session.Email !== undefined ? req.session.Email : "",
   });
 });
 
