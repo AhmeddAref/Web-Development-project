d17ad8cde2bc8c9c19f4b2492bba204bfeddcf2a
AhmedAref74 <125613887+AhmedAref74@users.noreply.github.com>
2023-06-06 21:33:31 +0300

    Update ajax-searchbar.js

--- Public/Js/ajax-searchbar.js
+++ Public/Js/ajax-searchbar.js
@@ -10,19 +10,34 @@ $(document).ready(function () {
       method: "POST",
       data: { searchfield: searchfield, selectedOption: selectedOption },
       success: function (data) {
+        // Clear the previous search results
+        $("#result").empty();
+
+        // Iterate over the data and create HTML elements for each result
         $.each(data, function (key, value) {
           if (
             value.name.search(expression) != -1 &&
             (selectedOption === "" || selectedOption === value.option)
           ) {
-            $("#result").append(
-              `<li class="list-group-item">
-                <img src="/images/${value.image1}" height="40" width="40" class="img-thumbnail" />
-                ${value.name}
-              </li>` +
-                value.name +
-                "  </li>"
-            );
+            // Create a new list item
+            var listItem = $("<li>", { class: "list-group-item" });
+
+            // Create an image element
+            var image = $("<img>", {
+              src: "/images/" + value.image1,
+              height: "40",
+              width: "40",
+              class: "img-thumbnail",
+            });
+
+            // Create a span element for the product name
+            var name = $("<span>").text(value.name);
+
+            // Append the image and name to the list item
+            listItem.append(image, name);
+
+            // Append the list item to the result container
+            $("#result").append(listItem);
           }
         });
       },
