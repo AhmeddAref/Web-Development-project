cbf752c10d5aaaedc5e9e8b01c58c09c3d290dc8
AhmedAref74 <125613887+AhmedAref74@users.noreply.github.com>
2023-06-07 01:14:15 +0300

    Update User.js

--- controllers/User.js
+++ controllers/User.js
@@ -1,19 +1,22 @@
 import { Router } from "express";
 import users from "../models/users.js";
-
 const getUser = (req, res) => {
-  users
-    .findOne({ email: req.body.email }) 
+    users.findOne(req.body.email)
     .then((result) => {
-      res.render("User-dashboard", {
-        user: result,
-        Email: req.session && req.session.Email !== undefined ? req.session.Email : "",
-        Type: req.session && req.session.Type !== undefined ? req.session.Type : "",
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
-
-export { getUser };
+          res.render("User-dashboard", {
+            user: result,
+            Email:
+              req.session && req.session.Email !== undefined
+                ? req.session.Email
+                : "",
+            Type:
+              req.session && req.session.Type !== undefined
+                ? req.session.Type
+                : "",
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    export { getUser };
